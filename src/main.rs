9ad450596739ba3f00ddd697f72e6433acf1a135
Pjiwm <pim.munne@gmail.com>
2022-06-28 02:16:06 +0200

    fixed bug removing new lines

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod scanner;
 mod token;
 #[macro_use]
 extern crate lazy_static;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 2 {
@@ -16,19 +17,16 @@ fn main() {
         process::exit(64);
     } else if args.len() == 2 {
         run_file(args[1].as_str()).unwrap();
+        println!("SRC: {:?}", args[1].as_str());
     } else {
         run_prompt().unwrap();
     }
 }
 
 fn run_file(path: &str) -> io::Result<()> {
-    let f = File::open(path)?;
-    let mut reader = BufReader::new(f);
-    let mut bytes = Vec::new();
-
-    // Read file into vector.
-    reader.read_to_end(&mut bytes)?;
-    run(str::from_utf8(&bytes).unwrap()).unwrap();
+    let src = std::fs::read_to_string(path)?;
+    print!("{}", src);
+    run(&src).unwrap();
     if error::get_error() {
         process::exit(65);
     }
@@ -54,7 +52,7 @@ fn run_prompt() -> io::Result<()> {
 }
 
 fn run(source: &str) -> io::Result<()> {
-    let mut token_scanner = scanner::Scanner::new(source.replace("\n", ""));
+    let mut token_scanner = scanner::Scanner::new(source.to_string());
     let tokens = token_scanner.scan_tokens();
     for token in tokens {
         println!("{:?}", token);
