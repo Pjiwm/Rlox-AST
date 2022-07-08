85cc27e72ad0edf3df49558accbb6abe8f9343a3
Pjiwm <pim.munne@gmail.com>
2022-07-08 15:34:31 +0200

    fixed borrow checker errors in parser

--- src/parser.rs
+++ src/parser.rs
@@ -14,91 +14,91 @@ impl Parser {
         Parser { tokens, current: 0 }
     }
 
-    fn expression(&self) -> Box<dyn Expr> {
+    fn expression(&mut self) -> Box<dyn Expr> {
         self.equality()
     }
 
-    fn equality(&self) -> Box<dyn Expr> {
-        let expr: Box<dyn Expr> = self.comparison();
+      fn equality(&mut self) -> Box<dyn Expr> {
+        let mut expr: Box<dyn Expr> = self.comparison();
         let equal_vec = vec![TokenType::Equalequal, TokenType::Bangequal];
-        while self.matches(equal_vec) {
-            let operator = self.previous();
+        while self.matches(&equal_vec) {
             let right = self.comparison();
-            let expr = Binary::new(expr, operator.clone(), right);
+            let operator = self.previous();
+            expr = Box::new(Binary::new(expr, operator.clone(), right));
         }
         expr
     }
 
-    fn comparison(&self) -> Box<dyn Expr> {
-        let expr: Box<dyn Expr> = self.term();
+    fn comparison(&mut self) -> Box<dyn Expr> {
+        let mut expr: Box<dyn Expr> = self.term();
         let comparison_vec = vec![
             TokenType::Greater,
             TokenType::Greaterequal,
             TokenType::Less,
             TokenType::Lessequal,
         ];
-        while self.matches(comparison_vec) {
-            let operator = self.previous();
+        while self.matches(&comparison_vec) {
             let right = self.term();
+            let operator = self.previous();
             expr = Box::new(Binary::new(expr, operator.clone(), right));
         }
         expr
     }
 
-    fn term(&self) -> Box<dyn Expr> {
-        let expr: Box<dyn Expr> = self.factor();
+    fn term(&mut self) -> Box<dyn Expr> {
+        let mut expr: Box<dyn Expr> = self.factor();
         let term_vec = vec![TokenType::Minus, TokenType::Plus];
-        while self.matches(term_vec) {
-            let operator = self.previous();
+        while self.matches(&term_vec) {
             let right = self.factor();
+            let operator = self.previous();
             expr = Box::new(Binary::new(expr, operator.clone(), right));
         }
         expr
     }
 
-    fn factor(&self) -> Box<dyn Expr> {
-        let expr: Box<dyn Expr> = self.unary();
+    fn factor(&mut self) -> Box<dyn Expr> {
+        let mut expr: Box<dyn Expr> = self.unary();
         let factor_vec = vec![TokenType::Star, TokenType::Slash];
-        while self.matches(factor_vec) {
-            let operator = self.previous();
+        while self.matches(&factor_vec) {
             let right = self.unary();
+            let operator = self.previous();
             expr = Box::new(Binary::new(expr, operator.clone(), right));
         }
         expr
     }
 
-    fn unary(&self) -> Box<dyn Expr> {
+    fn unary(&mut self) -> Box<dyn Expr> {
         let unary_vec = vec![TokenType::Bang, TokenType::Minus];
-        if self.matches(unary_vec) {
-            let operator = self.previous();
+        if self.matches(&unary_vec) {
             let right = self.unary();
+            let operator = self.previous();
             return Box::new(Unary::new(operator.clone(), right));
         }
         self.primary()
     }
 
-    fn primary(&self) -> Box<dyn Expr> {
+    fn primary(&mut self) -> Box<dyn Expr> {
         let false_vec = vec![TokenType::False];
-        if self.matches(false_vec) {
+        if self.matches(&false_vec) {
             return Box::new(Literal::new(Some(DataType::False(false))));
         } 
         let true_vec = vec![TokenType::True];
-        if self.matches(true_vec) {
+        if self.matches(&true_vec) {
             return Box::new(Literal::new(Some(DataType::True(true))));
         }
         let nil_vec = vec![TokenType::Nil];
-        if self.matches(nil_vec) {
+        if self.matches(&nil_vec) {
             return Box::new(Literal::new(None));
         }
 
         let data_type_vec = vec![TokenType::Number, TokenType::String];
-        if self.matches(data_type_vec) {
+        if self.matches(&data_type_vec) {
             let data_type = self.previous();
-            return Box::new(Literal::new(Some(data_type.literal.unwrap())));
+            return Box::new(Literal::new(Some(data_type.clone().literal.unwrap())));
         }
 
         let left_paren_vec = vec![TokenType::LeftParen];
-        if self.matches(left_paren_vec) {
+        if self.matches(&left_paren_vec) {
             let expr = self.expression();
             self.consume(TokenType::RightParen, "Expect ')' after expression.");
             return Box::new(Grouping::new(expr));
@@ -108,7 +108,7 @@ impl Parser {
     }
 
     // TODO implement consume
-    fn consume(&self, token_type: TokenType, message: &str) {
+    fn consume(&mut self, token_type: TokenType, message: &str) {
         if self.check(token_type) {
             self.advance();
         } else {
@@ -116,9 +116,9 @@ impl Parser {
         }
     }
 
-    fn matches(&self, types: Vec<TokenType>) -> bool {
+    fn matches(&mut self, types: &Vec<TokenType>) -> bool {
         for token_type in types {
-            if self.check(token_type) {
+            if self.check(token_type.clone()) {
                 self.advance();
                 return true;
             }
@@ -133,7 +133,7 @@ impl Parser {
         return self.peek().token_type == token_type;
     }
 
-    fn advance(&self) -> &Token {
+    fn advance(&mut self) -> &Token {
         if !self.is_at_end() {
             self.current += 1;
         }
