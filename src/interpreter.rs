6f72db0d75b31e6ee76bcb743e9f077d6d49d554
Pjiwm <pim.munne@gmail.com>
2022-07-13 20:28:28 +0200

    binary expression in interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,6 +1,6 @@
 use crate::{
     expr::{self, *},
-    token::{DataType, TokenType},
+    token::{self, DataType, TokenType},
 };
 
 pub struct Interpreter {}
@@ -11,7 +11,48 @@ impl ExprVisitor for Interpreter {
     }
 
     fn visit_binary_expr(&mut self, expr: &Binary) -> ReturnTypes {
-        todo!()
+        // TODO panics could be replaced with a better error handling system
+        let left = match expr.left.accept(self) {
+            ReturnTypes::DataType(d) => d,
+            _ => panic!("Expected a number"),
+        };
+
+        let right = match expr.right.accept(self) {
+            ReturnTypes::DataType(d) => d,
+            _ => panic!("Expected a number"),
+        };
+
+        let calculation = match expr.operator.token_type {
+            // There's extra logic for strings, this is so strings can be concatinated with the + operator.
+            TokenType::Plus => match (left, right) {
+                (Some(DataType::Number(l)), Some(DataType::Number(r))) => DataType::Number(l + r),
+                (Some(DataType::String(l)), Some(DataType::String(r))) => {
+                    let mut s = String::new();
+                    s.push_str(l.as_str());
+                    s.push_str(r.as_str());
+                    DataType::String(s)
+                }
+                _ => panic!("Expected a number or string"),
+            },
+            TokenType::Minus => match (left, right) {
+                (Some(DataType::Number(l)), Some(DataType::Number(r))) => DataType::Number(l - r),
+                _ => panic!("Expected a number"),
+            },
+            TokenType::Slash => match (left, right) {
+                (Some(DataType::Number(l)), Some(DataType::Number(r))) => DataType::Number(l / r),
+                _ => panic!("Expected a number"),
+            },
+            TokenType::Star => match (left, right) {
+                (Some(DataType::Number(l)), Some(DataType::Number(r))) => DataType::Number(l * r),
+                _ => panic!("Expected a number"),
+            },
+            TokenType::Equalequal => match (left, right) {
+                (Some(l), Some(r)) => DataType::Bool(self.is_equal(&l, &r)),
+                _ => panic!("Expected a binary operation"),
+            },
+            _ => panic!("No such binary operator"),
+        };
+        ReturnTypes::DataType(Some(calculation))
     }
 
     fn visit_call_expr(&mut self, expr: &Call) -> ReturnTypes {
@@ -57,7 +98,6 @@ impl ExprVisitor for Interpreter {
         };
         match expr.operator.token_type {
             TokenType::Minus => ReturnTypes::DataType(Some(right)),
-            // TODO fix this tomorrow
             TokenType::Bang => ReturnTypes::DataType(Some(DataType::Bool(!self.is_truthy(&right)))),
             _ => todo!(),
         }
@@ -74,6 +114,16 @@ impl Interpreter {
             DataType::Bool(b) => *b,
             DataType::Nil => false,
             _ => true,
+        }
+    }
+
+    fn is_equal(&self, a: &DataType, b: &DataType) -> bool {
+        match (a, b) {
+            (DataType::Number(a), DataType::Number(b)) => a == b,
+            (DataType::String(a), DataType::String(b)) => a == b,
+            (DataType::Bool(a), DataType::Bool(b)) => a == b,
+            (DataType::Nil, DataType::Nil) => true,
+            _ => false,
         }
     }
 }
