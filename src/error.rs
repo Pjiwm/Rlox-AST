e7858e5b145a6c4984c28d9a64509e9970ad0ecc
Pjiwm <pim.munne@gmail.com>
2022-07-14 18:54:20 +0200

    removed line location for error logging

--- src/error.rs
+++ src/error.rs
@@ -2,25 +2,19 @@ use crate::token::{Token, TokenType};
 
 static mut HAD_ERROR: bool = false;
 pub fn error(line: u32, message: &str) {
-    report(line, "", message);
+    report(line, message);
 }
 
 pub fn token_error(token: &Token, message: &str) {
     if token.token_type == TokenType::Eof {
-        report(token.line, " at end", message);
+        report(token.line, message);
     } else {
-        report(
-            token.line,
-            format!(" at '{}'", &token.lexeme).as_str(),
-            message,
-        );
+        report(token.line, message);
     }
 }
 
-fn report(line: u32, location: &str, message: &str) {
-    let location = if location.is_empty() { "0" } else { location };
-    if location.is_empty() {}
-    println!("Error at {}-{}: {}", line, location, message);
+fn report(line: u32, message: &str) {
+    println!("Error at line {}: {}", line, message);
     println!("{}", message);
     set_error(true);
 }
